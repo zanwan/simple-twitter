de4a420f9d4e0e389de10de3b4fe9b6a4fa5a557
v123582 <v123582@gmail.com>
2019-06-24 01:08:20 +0800

    update migrations

--- migrations/20190115071419-create-like.js
+++ migrations/20190115071419-create-like.js
@@ -8,10 +8,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userId: {
+      UserId: {
         type: Sequelize.INTEGER
       },
-      tweetId: {
+      TweetId: {
         type: Sequelize.INTEGER
       },
       createdAt: {
