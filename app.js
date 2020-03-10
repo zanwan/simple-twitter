5d294ede84275cd1535542e9d86c2ee4db05ca4d
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-03-10 11:44:25 +0800

    Initalize project & introduce the handlebars

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const helpers = require('./_helpers');
+const express = require("express");
+const helpers = require("./_helpers");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+// 引入 handlebars
+const handlebars = require("express-handlebars");
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+// Handlebars 註冊樣板引擎
+app.engine("handlebars", handlebars());
+// 設定使用 Handlebars 做為樣板引擎
+app.set("view engine", "handlebars");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-module.exports = app
+module.exports = app;
