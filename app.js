029b0087ffb0147d58d6117f8d958854822b598f
zangwang <info@zangwang.tw>
2020-03-10 16:37:52 +0800

    :lipstick: Add main / homepage / replies handlebars view
    詳述：
    完成 main.handlebars
    完成 tweetHome.handlebars (首頁)
    完成 replies.handlebars （回覆頁）
    路由 app.js使用"/"當預覽view的暫時路由
    新增 pucblic/stylesheet/style.css
    
    備忘：
    今天要完成所有基本視圖

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const helpers = require('./_helpers');
+const express = require("express")
+const helpers = require("./_helpers")
+const handlebars = require("express-handlebars")
 
 const app = express()
 const port = 3000
@@ -7,7 +8,13 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//設定樣版引擎
+app.engine("handlebars", handlebars()) //註冊樣版引擎
+app.set("view engine", "handlebars") //設定樣板引擎
+app.use(express.static("public")) //讀取靜態檔案
+//前端視圖暫時路由
+app.get("/", (req, res) => res.render("replies"))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
