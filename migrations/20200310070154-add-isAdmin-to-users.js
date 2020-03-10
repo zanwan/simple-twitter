63a837f2c903ddb173b7b7f9b371f87a2dbb88ce
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-03-10 15:06:10 +0800

    Add isAdmin to users table & Add authentication before rendering pages

---
+++ migrations/20200310070154-add-isAdmin-to-users.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn("Users", "isAdmin", {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn("Users", "isAdmin");
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,31 @@ const restController = require("../controllers/restController.js");
 const adminController = require("../controllers/adminController.js");
 const userController = require("../controllers/userController.js");
 module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/signin");
+  };
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next();
+      }
+      return res.redirect("/");
+    }
+    res.redirect("/signin");
+  };
   // 記得這邊要接收 passport
   // 如果使用者訪問首頁，就導向 /restaurants 的頁面
-  app.get("/", (req, res) => res.redirect("restaurants"));
-  app.get("/restaurants", restController.getRestaurants);
+  app.get("/", authenticated, (req, res) => res.redirect("restaurants"));
+  app.get("/restaurants", authenticated, restController.getRestaurants);
 
   // 連到 /admin 頁面就轉到 /admin/restaurants
-  app.get("/admin", (req, res) => res.redirect("/admin/restaurants"));
-  app.get("/admin/restaurants", adminController.getRestaurants);
+  app.get("/admin", authenticated, (req, res) =>
+    res.redirect("/admin/restaurants")
+  );
+  app.get("/admin/restaurants", authenticated, adminController.getRestaurants);
 
   app.get("/signup", userController.signUpPage);
   app.post("/signup", userController.signUp);
