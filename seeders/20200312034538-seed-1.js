149575d8ed7873063d26baab340218b87c36689d
Chris <shian0222@gmail.com>
2020-03-12 12:02:08 +0800

    Add seeder fake data

---
+++ seeders/20200312034538-seed-1.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.bulkInsert('Users', [{
+      email: 'root@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: "root",
+      introduction: faker.lorem.text(),
+      avatar: faker.image.imageUrl(),
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: "user1",
+      introduction: faker.lorem.text(),
+      avatar: faker.image.imageUrl(),
+      role: 'user',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: "user2",
+      introduction: faker.lorem.text(),
+      avatar: faker.image.imageUrl(),
+      role: 'user',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+    queryInterface.bulkInsert('Tweets',
+      Array.from({ length: 50 }).map(_d =>
+        ({
+          UserId: Math.floor(Math.random() * 3) + 1,
+          description: faker.lorem.text(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      ), {});
+    queryInterface.bulkInsert('Likes',
+      Array.from({ length: 50 }).map(_d =>
+        ({
+          UserId: Math.floor(Math.random() * 3) + 1,
+          TweetId: Math.floor(Math.random() * 50) + 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      ), {});
+    return queryInterface.bulkInsert('Replies',
+      Array.from({ length: 150 }).map(_d =>
+        ({
+          comment: faker.lorem.sentence(),
+          UserId: Math.floor(Math.random() * 3) + 1,
+          TweetId: Math.floor(Math.random() * 50) + 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      ), {});
+
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('Users', null, {})
+    queryInterface.bulkDelete('Replies', null, {})
+    return queryInterface.bulkDelete('Tweets', null, {})
+  }
+};
